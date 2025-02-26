13e13c0a483cb1fdced3f59ed942575f704b12e6
Saief <A.S.Bokor@outlook.com>
2025-02-26 13:23:39 +0000

    Task modal open function created

--- src/DOM.js
+++ src/DOM.js
@@ -1,1 +1,24 @@
 // DOM.js
+
+//Import
+import { myTasks, Task, validateForm, addTask, removeFromArray } from "./logic";
+
+// Collect DOM elements
+const addBtn = document.querySelector(".contentAddButton");
+const modal = document.querySelector(".formSection")
+const overlay =document.querySelector(".overlay")
+
+
+// Modal open function
+function addTaskButton() {
+    addBtn.onclick = function () {
+        console.log("-----addTaskButton function initiated-----");
+        console.log("addTaskButton clicked");
+        modal.style.display = "block";
+        overlay.style.display = "block";
+        console.log("taskModal displayed and overlay added");
+    }
+}
+
+export {addTaskButton};
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 //index.js
 
 import "./styles.css";
+import { addTaskButton } from "./DOM";
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("-----DOM Loaded-----");
+    addTaskButton();
 });
