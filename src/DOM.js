edb058c2eb0e2bbbda331f69e9417b25cc319735
Saief <A.S.Bokor@outlook.com>
2025-02-26 13:56:54 +0000

    began working on the task submit button function

--- src/DOM.js
+++ src/DOM.js
@@ -5,11 +5,11 @@ import { myTasks, Task, validateForm, addTask, removeFromArray } from "./logic";
 
 // Collect DOM elements
 const addBtn = document.querySelector(".contentAddButton");
-const modal = document.querySelector(".formSection")
-const overlay =document.querySelector(".overlay")
+const modal = document.querySelector(".formSection");
+const overlay =document.querySelector(".overlay");
 const closeBtn = document.querySelector(".modalClose");
 const modalCancel = document.querySelector(".cancelBtn");
-
+const submitBtn = document.querySelector(".submit");
 
 // Modal open function
 function addTaskButton() {
@@ -44,7 +44,26 @@ function cancelButton() {
     };
 };
 
+/* const taskInput = document.querySelector(".task");
+const descriptionInput = document.querySelector(".description");
+const dateInput = document.querySelector(".date");
+const priorityInput = document.querySelector(".priority");
+const projectInput = document.querySelector(".projectInput");
 
+// Submit button function
+function submitButton() {
+    submitBtn.onclick = function (event) {
+        console.log("-----submitButton function initiated-----");
+        console.log("submitBtn clicked");
+        modal.style.display = "none";
+        overlay.style.display = "none";
+        console.log("taskModal hidden and overlay removed");
+        event.preventDefault();
+        const task = taskInput.value;
 
+    }
+} */
+
+// Export
 export {addTaskButton, closeButton, cancelButton};
 
